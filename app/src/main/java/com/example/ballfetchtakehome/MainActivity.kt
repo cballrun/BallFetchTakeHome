5e03871b95d386c50725c8d0b91ea6f9ee1bd82c
cballrun <carter.k.ball@gmail.com>
2024-10-03 02:39:56 -0400

    Add coroutine to get items and update RecyclerView after successful response

--- app/src/main/java/com/example/ballfetchtakehome/MainActivity.kt
+++ app/src/main/java/com/example/ballfetchtakehome/MainActivity.kt
@@ -1,19 +1,58 @@
 package com.example.ballfetchtakehome
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.ballfetchtakehome.adapters.RvAdapter
 import com.example.ballfetchtakehome.databinding.ActivityMainBinding
+import com.example.ballfetchtakehome.models.FetchItem
+import com.example.ballfetchtakehome.utils.RetrofitInstance
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import retrofit2.HttpException
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
   private lateinit var binding: ActivityMainBinding
+  private lateinit var rvAdapter: RvAdapter
+  private lateinit var fetchList: List<FetchItem>
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     binding = ActivityMainBinding.inflate(layoutInflater)
     enableEdgeToEdge()
     setContentView(binding.root)
+
+    fetchList = listOf()
+
+    GlobalScope.launch(Dispatchers.IO) {
+      val response = try {
+        RetrofitInstance.api.getAllFetch()
+      }catch(e: IOException){
+        Toast.makeText(applicationContext, "Application error ${e.message}", Toast.LENGTH_LONG).show()
+        return@launch
+      }catch(e: HttpException){
+        Toast.makeText(applicationContext, "HTTP error ${e.message}", Toast.LENGTH_LONG).show()
+        return@launch
+      }
+      if(response.isSuccessful && response.body() != null) {
+        withContext(Dispatchers.Main){
+          fetchList = response.body()!!
+          binding.rvMain.apply{
+            rvAdapter = RvAdapter(fetchList)
+            adapter = rvAdapter
+            layoutManager = LinearLayoutManager(this@MainActivity)
+          }
+        }
+      }
+    }
+
     ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
       val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
       v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
