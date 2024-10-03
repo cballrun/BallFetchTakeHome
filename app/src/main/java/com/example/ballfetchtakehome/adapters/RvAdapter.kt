39e11a8caf2d522464f5814bba64375bd42a49d0
cballrun <carter.k.ball@gmail.com>
2024-10-03 02:24:09 -0400

    Initialize RecyclerView adapter with fetchList, extend RecyclerView.adapter, add getItemCount and onBindViewHolder function placeholders

--- app/src/main/java/com/example/ballfetchtakehome/adapters/RvAdapter.kt
+++ app/src/main/java/com/example/ballfetchtakehome/adapters/RvAdapter.kt
@@ -4,16 +4,25 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.ballfetchtakehome.databinding.ItemLayoutBinding
+import com.example.ballfetchtakehome.models.FetchItem
 
-class RvAdapter {
+class RvAdapter (private val fetchList: List<FetchItem>): RecyclerView.Adapter<RvAdapter.ViewHolder>(){
   inner class ViewHolder(val binding: ItemLayoutBinding) :
       RecyclerView.ViewHolder(binding.root){
 
       }
 
-  fun onCreateViewHolder(parent: ViewGroup, ViewType: Int): ViewHolder{
+  override fun onCreateViewHolder(parent: ViewGroup, ViewType: Int): ViewHolder{
     return ViewHolder(
       ItemLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
     )
+  }
+
+  override fun getItemCount(): Int {
+    TODO("Not yet implemented")
+  }
+
+  override fun onBindViewHolder(holder: ViewHolder, position: Int){
+
   }
 }
